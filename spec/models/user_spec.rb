f9263eb78f7a9c7c9c9fdc88af1cf269a3440373
Allu Balakrishna <balakrishna@carmatec.com>
2016-07-11 15:50:00 +0530

    Done

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,7 +8,7 @@ describe User do
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email) }
   it { should validate_confirmation_of(:password) }
-  it { should allow_value('example@domain.com').for(:email)}
+  it { should allow_value('example@domain.com').for(:email) }
 
   it { should be_valid }
 
